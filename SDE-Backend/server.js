3319d85df6955df8b5e7d328746ff8329626b4b9
sivateja510 <85979901+sivateja510@users.noreply.github.com>
2023-09-03 21:24:56 +0530

    Add files via upload

---
+++ SDE-Backend/server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const cors=require('cors');
+const monk=require('monk');
+const server = express();
+const PORT = 8090;
+server.use(cors());
+server.use(express.json());
+const urll=('mongodb+srv://sivateja:sivateja@facultydairy.wqm3eo7.mongodb.net/data');
+// const urll=('mongodb://localhost:27017/demo')
+const db=monk(urll);
+const entr=db.get('ques');
+db.then(()=>{
+    console.log("db connected");
+})
+server.get('/',(req,res)=>{
+    res.status(200);
+    res.send(" server is running ");
+});
+
+server.get('/entry',(req,res)=>
+{
+    entr.find({}).then((docs)=>
+    {
+        res.json(docs);
+    });
+})
+const topic=db.get('DSA');
+server.get('/DSA',(req,res)=>
+{
+    topic.find({}).then((docs)=>
+    {
+        res.json(docs);
+    });
+})
+
+const algos=db.get('Algos');
+server.get('/Algos',(req,res)=>
+{
+    algos.find({}).then((docs)=>
+    {
+        res.json(docs);
+    });
+})
+const sqll=db.get('SQL');
+server.get('/SQL',(req,res)=>
+{
+    sqll.find({}).then((docs)=>{
+        res.json(docs);
+    });
+})
+
+const nsqll=db.get('NoSql');
+server.get('/NoSql',(req,res)=>
+{
+    nsqll.find({}).then((docs)=>{
+        res.json(docs);
+    });
+})
+const os=db.get('Operating-Systems');
+server.get('/Operating-Systems',(req,res)=>
+{
+    os.find({}).then((docs)=>
+    {
+        res.json(docs);
+    });
+})
+
+const practice=db.get('Practice');
+server.get('/Practice',(req,res)=>
+{
+    practice.find({}).then((docs)=>
+    {
+        res.json(docs);
+    });
+})
+
+const disc=db.get('Discussions');
+server.get('/Discussions',(req,res)=>
+{
+    disc.find({}).then((docs)=>
+    {
+        res.json(docs);
+    });
+})
+
+const osa=db.get('OsAlgos');
+server.get('/OsAlgos',(req,res)=>
+{
+    osa.find({}).then((docs)=>
+    {
+        res.json(docs);
+    });
+})
+
+  
+server.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running,and server is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
